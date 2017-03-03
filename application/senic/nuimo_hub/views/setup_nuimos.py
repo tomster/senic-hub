db0ea06c77a99d0be2eb77ff45603f5a43797cfe
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-03 17:10:06 +0100

    Add nuimo setup starter endpoint

--- application/senic/nuimo_hub/views/setup_nuimos.py
+++ application/senic/nuimo_hub/views/setup_nuimos.py
@@ -2,16 +2,40 @@ from cornice.service import Service
 from os import path
 # TODO: We better rename `config.path` to something else. Conflicts with `os.path`
 from ..config import path as service_path
+from ..subprocess_run import run
 
 
-nuimo_setup = Service(
-    name='nuimo_setup',
+nuimo_setup_starter = Service(
+    name='start_nuimo_setup',
+    path=service_path('setup/nuimos/start'),
+    renderer='json',
+    accept='application/json')
+
+
+connected_nuimos = Service(
+    name='connected_nuimos',
     path=service_path('setup/nuimos'),
     renderer='json',
     accept='application/json')
 
 
-@nuimo_setup.get()
+# TODO: Remove get()
+@nuimo_setup_starter.get()
+@nuimo_setup_starter.post(renderer='json')
+def start_nuimo_setup(request):
+    # TODO: Collect config file from some settings
+    # TODO: Collect Bluetooth adapter name from some settings
+    # TODO: Should be started via supervisorctl to avoid multiple instances running
+    run([
+        'sudo',
+        path.join(request.registry.settings['fs_bin'], 'setup_nuimo'),
+        '-c', '/srv/nuimo_hub/production.ini',
+        'hci0'
+    ])
+    return {}
+
+
+@connected_nuimos.get()
 def get_nuimo_mac_address(request):
     nuimo_mac_address_filepath = request.registry.settings.get('nuimo_mac_address_filepath')
     if not path.exists(nuimo_mac_address_filepath):
